1fc78f4048fc593c5c28d0d01e9cfd6fed0b733e
anhtuansggd <122171727+anhtuansggd@users.noreply.github.com>
2024-10-21 19:37:22 +0700

    fix: product.controller
    - miss product_shop in clothes document
    - different _id of clothes and products

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,12 +1,16 @@
 'use strict'
 const ProductService = require("../services/product.service")
 const {CREATED, SuccessResponse} = require("../core/success.response");
+const {product} = require("../models/product.model");
 class AccessController{
     createProduct = async (req, res, next) => {
+        console.log(req.user)
         new SuccessResponse({
             message: 'Create new Product success',
-
-            metadata: await ProductService.createProduct(req.body.product_type, req.body)
+            metadata: await ProductService.createProduct(req.body.product_type, {
+                ...req.body,
+                product_shop: req.user.userId
+            })
         }).send(res)
     }
 
